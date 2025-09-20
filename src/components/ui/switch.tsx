2df0a2477574f9e07fa130c41a36873343b947db
Ooozari <uzairasiff1227@gmail.com>
2025-09-20 16:29:07 +0500

    `style: reduce switch size for a more compact UI`

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -17,8 +17,8 @@ function Switch({
       checked={checked}
       onCheckedChange={onCheckedChange}
       className={cn(
-        "peer inline-flex h-[20px] w-[36px] md:h-6 md:w-12 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-colors",
-        "bg-switch-bg",
+        "peer inline-flex h-[16px] w-[30px] md:h-[20px] md:w-[36px] shrink-0 items-center rounded-full border border-transparent shadow-xs transition-colors",
+        "bg-page-bg",
         "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary",
         "disabled:cursor-not-allowed disabled:opacity-50",
         className
@@ -28,10 +28,9 @@ function Switch({
       <SwitchPrimitive.Thumb
         className={cn(
           "pointer-events-none flex items-center justify-center",
-          "h-5 w-5 transition-transform duration-300 bg-none",
-          // mobile (<md) translate shorter, md+ translate longer
-          "data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0",
-          "md:data-[state=checked]:translate-x-6 md:data-[state=unchecked]:translate-x-[2px]"
+          "h-4 w-4 transition-transform duration-300 bg-none",
+          "data-[state=checked]:translate-x-3 data-[state=unchecked]:translate-x-0",
+          "md:data-[state=checked]:translate-x-[18px] md:data-[state=unchecked]:translate-x-[1px]"
         )}
       >
         {checked ? (
